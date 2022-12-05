0884da96585002d269ae5b3195896224233250bf
Suat Erken <erkensuat@gmail.com>
2022-12-05 22:32:09 +0100

    build update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { ContentAssetPlugin } from './content-asset-plugin'
+export * from './content-asset-plugin'
 export * from './content-asset-config.entity'
 export * from './content-asset.resolver'
 export * from './content-asset.graphql'
